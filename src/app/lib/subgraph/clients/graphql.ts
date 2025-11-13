cc2c0360720e37d79a1da2b6a98661b56fd95b30
emmajane1313 <emma.jane@digitalax.xyz>
2025-11-13 01:06:53 -0500

    node

--- src/app/lib/subgraph/clients/graphql.ts
+++ src/app/lib/subgraph/clients/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const fgoFuturesLink = new HttpLink({
-  uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/En2vb9DWdZ7Qpw1e111Rb3jGR2oWEDN2dB2YnPuhw21h`,
+  uri: "/api/graphql/futures",
 });
 
 export const graphFGOFuturesClient = new ApolloClient({
@@ -9,14 +9,11 @@ export const graphFGOFuturesClient = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
 const fgoLink = new HttpLink({
-  uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/4jujS6j8wMahNTSzN2T8wN7iTiEfNRQfoYYQL7Q1Bn9X`,
+  uri: "/api/graphql/fgo",
 });
 
 export const graphFGOClient = new ApolloClient({
   link: fgoLink,
   cache: new InMemoryCache(),
 });
-
-
